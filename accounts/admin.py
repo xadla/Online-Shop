37aa4e6d88b9f2ce7baf9f04fa54696914d887cb
xadla <hadinjr4122@gmail.com>
2025-03-16 16:57:49 +0330

    change admin panel and create products app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
 
 
 from .models import User
+from .forms import UserCreationForm, UserChangeForm
 
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+
+    form =  UserChangeForm
+    add_form = UserCreationForm
+
+
+    list_display = ["email", "is_admin", "phone_number"]
+    list_filter = ["is_admin"]
+
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Information", {"fields": ["first_name", "last_name", "address", "phone_number", "last_login"]}),
+        ("Permission", {"fields": ["is_admin"]})
+    ]
+
+    add_fieldsets = [
+        (
+            None,
+            {
+                "fields": ["email", "first_name", "last_name", "password1", "password2"],
+            },
+        )
+    ]
+
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
